626edfeb6a7ee218e6e55b1adc1322aeea7d7b6a
mjh9401 <mjh9401@naver.com>
2021-11-10 18:22:59 +0900

    시범 홈화면

--- src/main/java/com/mjh/exam/projoect1/controller/UserHomeController.java
+++ src/main/java/com/mjh/exam/projoect1/controller/UserHomeController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UserHomeController {
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "안녕하세요";
+		return "usr/home/home";
 	}
+	
+	@RequestMapping("/")
+	public String returnMain() {
+		return "redirect:/usr/home/main";
+	}
+	
+	
 }
---
+++ src/main/webapp/WEB-INF/jsp/usr/home/home.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file = "../common/header.jspf" %>
+ <style>
+  body{
+    background-image:url("/resource/img/01.jpg");
+    background-repeat:no-repeat;
+    background-size: cover;
+  }
+</style>
+  
+ 
+<%@ include file = "../common/footer.jspf"%>
